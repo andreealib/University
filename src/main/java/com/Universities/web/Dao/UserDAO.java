15fc00b9ebd3bd7b15343996bf8d60633c1b99f8
Andreea Libotean <andreea.libotean@yahoo.com>
2015-12-02 11:05:03 +0200

    spring security working

--- src/main/java/com/Universities/web/Dao/UserDAO.java
+++ src/main/java/com/Universities/web/Dao/UserDAO.java
@@ -4,6 +4,7 @@ import com.Universities.web.data.User;
 import org.hibernate.Query;
 import org.hibernate.classic.Session;
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,7 +20,7 @@ import java.util.List;
 public class UserDAO {
 
     @Autowired
-    private HibernateTemplate hibernateTemplate;
+    private  org.springframework.orm.hibernate3.HibernateTemplate hibernateTemplate;
 
     private Session getSession() {
         return hibernateTemplate.getSessionFactory().getCurrentSession();
