ece735d86e5be453769fcbd07b6bd7259527775f
Andreea Libotean <andreea.libotean@yahoo.com>
2015-12-03 15:04:28 +0200

    customized 403-access denied page

--- src/main/java/com/Universities/web/Controller/LinkNavigation.java
+++ src/main/java/com/Universities/web/Controller/LinkNavigation.java
@@ -2,9 +2,12 @@ package com.Universities.web.Controller;
 
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
 
 /**
  * Created by andreealibotean on 11/27/2015.
@@ -12,31 +15,39 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class LinkNavigation {
 
-    @RequestMapping(value="/", method= RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView homePage() {
 
         return new ModelAndView("home");
     }
 
-    @RequestMapping(value="/index", method=RequestMethod.GET)
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
     public ModelAndView indexPage() {
 
         return new ModelAndView("home");
     }
 
 
-    @RequestMapping(value="/sec/moderation", method=RequestMethod.GET)
+    @RequestMapping(value = "/sec/moderation", method = RequestMethod.GET)
     public ModelAndView moderatorPage() {
 
         return new ModelAndView("moderation");
     }
 
 
-    @RequestMapping(value="/admin/first", method=RequestMethod.GET)
+    @RequestMapping(value = "/admin/first", method = RequestMethod.GET)
     public ModelAndView firstAdminPage() {
 
         return new ModelAndView("admin-first");
     }
 
 
+    @RequestMapping(value = "/403", method = RequestMethod.GET)
+    public ModelAndView accesssDenied() {
+
+        ModelAndView model = new ModelAndView();
+        model.setViewName("403");
+        return model;
+
+    }
 }
--- src/main/java/com/Universities/web/services/CustomUserDetailsService.java
+++ src/main/java/com/Universities/web/services/CustomUserDetailsService.java
@@ -25,7 +25,6 @@ import java.util.List;
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
-    //@Resource(name="userDAO")
     private UserDAO userDAO;
 
 
